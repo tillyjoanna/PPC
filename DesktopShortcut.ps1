d8b7b32a0b7276af42539dfe6d36146d36c0bf1e
tillyjoanna <tillyjoanna@gmail.com>
2025-03-26 14:21:41 +0000

    Update DesktopShortcut.ps1

--- DesktopShortcut.ps1
+++ DesktopShortcut.ps1
@@ -1,6 +1,6 @@
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Notepad++.lnk")
-$Shortcut.TargetPath="C:\Program Files\Notepad++"
+$Shortcut.TargetPath="C:\Program Files\Notepade++\notepad++.exe"
 $Shortcut.Arguments="/l"
 $Shortcut.IconLocation="C:\Program Files\Notepad++\notepad++.exe"
 $Shortcut.Save()
